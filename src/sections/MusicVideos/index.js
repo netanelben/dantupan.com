13131b85a3ef61364598803e8dd0b04a0bf0f230
netanelben <mtr574@gmail.com>
2020-10-17 21:00:43 +0300

    -

--- src/sections/MusicVideos/index.js
+++ src/sections/MusicVideos/index.js
@@ -5,9 +5,9 @@ const MusicVideos = () => (
 
         <iframe width="560" height="315"
             src="https://www.youtube.com/embed/d-KDnYT8aks"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen/>
+            allowFullScreen/>
     </div>
 );
 
