87651974f9fb0ebfa712b563a66af0c5f590f3ab
netanelben <mtr574@gmail.com>
2020-12-10 18:51:42 +0200

    -

--- src/App.js
+++ src/App.js
@@ -7,8 +7,22 @@ import './App.scss';
 
 const App = () => (
     <div className="main">
-        <h1>thetupan</h1>
-        <Container fluid>
+        <h1 style={{ fontSize: '58px' }}>Dan Tupan</h1>
+        <iframe
+            allowTransparency="true"
+            scrolling="no" frameBorder="no"
+            src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fuser-446627522&color=white_orange&size=64"
+            style={{ width: '64px', height: '64px' }}/>
+
+        <div style={{ paddingTop: '10em' }}>
+            <p style={{ textAlign: 'center', fontSize: '18px', color: 'purple' }}>
+                ⌰⟊⎅⎎⊑⏃⌰☍⌇⊑⟊⎅⌰☍⏃⟊⌇⎅⌰☍⏃⟊⌇⎅☍⌰⏃⟊⌇⎅
+                <br/>
+                ⏃⟊⋏⌇☍⎅⊑⏃⌇⌰☍⟊⎅⊑⏃☍⌇⎅⊑☍⏃⟊⌇⊑⎅
+            </p>
+            {/* https://lingojam.com/AlienLanguage */}
+        </div>
+        {/* <Container fluid>
             <Row>
                 <Col sm="12" md="4">
                     <ImageGallery/>
@@ -20,7 +34,7 @@ const App = () => (
                     <Community/>
                 </Col>
             </Row>
-        </Container>
+        </Container> */}
     </div>
 );
 
